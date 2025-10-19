0eca71c0a2519086cc1f8502f846488566e12a82
Bryce Blanton <bryce@koddi.com>
2025-10-19 13:44:56 -0500

    Fix .gitignore and include cmd/gspro-control

---
+++ cmd/gspro-control/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/bblanton/gspro-control/internal/server"
+)
+
+func main() {
+	addr := ":8080"
+	if v := os.Getenv("PORT"); v != "" {
+		addr = ":" + v
+	}
+
+	mux := http.NewServeMux()
+	server.RegisterRoutes(mux)
+
+	log.Printf("GSPro Control API listening on %s", addr)
+	if err := http.ListenAndServe(addr, withCORS(mux)); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
+}
+
+func withCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
